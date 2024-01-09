b79a57c1dce320d3cd34457aa2743ae1696499fd
kimba <skyqwn@gmail.com>
2024-01-09 23:09:26 +0900

    nest api

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+  @Get('/')
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('/search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie iwth a title: ${searchingYear} `;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: number): Movie {
+    return this.movieService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
